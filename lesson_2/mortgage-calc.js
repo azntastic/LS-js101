265b4c03d73f4fbdb24b880f666f1351f1edd7bc
Ryan Chu <ryanchu.ws@gmail.com>
2020-07-20 16:21:41 -1000

    finished mortgage calculator

--- lesson_2/mortgage-calc.js
+++ lesson_2/mortgage-calc.js
@@ -1,18 +1,76 @@
+// Dependencies
 const readline = require('readline-sync');
 
-console.log('Input your loan amount');
-let loanAmount = readline.question();
+// Functions
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
 
-console.log('Input APR');
-let apr = Number(readline.question()) / 100;
+function invalidNumber(number) {
+  /*
+  Number() coercion function trims whitespace automatically,
+  and second condition makes sure that empty user input,
+  which is coerced into 0, will fail this validation.
 
-console.log('Input duration (in years)');
-let lengthYears = readline.question();
+  Would it be better to follow the solution & write a condition
+  that explicitly targets case where user inputs nothing or only
+  whitespace characters?
+  */
+  return Number.isNaN(number) || number < 0;
+}
 
-let monthlyRate = apr / 12;
-let lengthMonths = lengthYears * 12;
+// Main
 
-let monthlyPayment = loanAmount * (monthlyRate /
-(1 - Math.pow((1 + monthlyRate), (-lengthMonths))));
+// Declare variables
+let apr, lengthYears, monthlyRate, lengthMonths, monthlyPayment;
 
-console.log(monthlyPayment);
+// Main Program Loop
+while (true) {
+
+  // Get loan amount
+  prompt('Input your loan amount (digits, no special characters)');
+  loanAmount = Number(readline.question());
+    // Validation
+    while (invalidNumber(loanAmount)) {
+      prompt('Invalid input. Please input numbers only');
+      loanAmount = Number(readline.question());
+    }
+  // Get APR
+  prompt('Input APR (if the interest rate is 5%, input 5)');
+  apr = Number(readline.question()) / 100;
+    // Validation
+    while (invalidNumber(apr)) {
+      prompt('Invalid input. Please input numbers only');
+      apr = Number(readline.question());
+    }
+  // Get loan duration
+  prompt('Input loan duration (in years)');
+  lengthYears = Number(readline.question());
+    // Validation
+    while (invalidNumber(lengthYears)) {
+      prompt('Invalid input. Please input numbers only');
+      lengthYears = Number(readline.question());
+    }
+
+  // Processing variables for use in equation
+  monthlyRate = apr / 12;
+  lengthMonths = lengthYears * 12;
+
+  // Calculate monthly payment
+  monthlyPayment = loanAmount * (monthlyRate /
+  (1 - Math.pow((1 + monthlyRate), (-lengthMonths))));
+
+  // Output
+  prompt(`Your monthly payment is $${monthlyPayment.toFixed(2)}`);
+  prompt('Would you like to run another calculation? (y/n)');
+  runAnother = readline.question().toLowerCase();
+
+  // Validate user reponse
+  while (runAnother !== 'y' && runAnother !== 'n') {
+    prompt('invalid input. Please input either y or n');
+    runAnother = readline.question().toLowerCase();
+  }
+
+  // Run again (or not)
+  if (runAnother[0] === 'n') break;
+}
